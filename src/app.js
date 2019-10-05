de318c6f2025f0c5055c41b8ac201ad325b8f864
Ming Luo <luoming886@gmail.com>
2019-10-05 19:53:10 +0800

    created Header and Actioncomponents

--- src/app.js
+++ src/app.js
@@ -1,43 +1,35 @@
-console.log('App.js is running');
 
-//JSX- JavaScript XML
-var template = (
-    <div>
-        <h1>does change?</h1> 
-        <p>This is some info</p>
-        <ol>
-            <li>Item 1</li>
-            <li>Item 2</li>
-        </ol>
-        <form>
-           <input type="text" name="option"></input>
-           <button>Add Option</button>
-        </form>
-    </div>
-);
+class Header extends React.Component{
+    render(){
 
-var userName = 'Ming';
-var userAge = 27;
-var userLocation = 'New York';
+        return (
+            <div>
+                <h1>DecisionMaker</h1>
+                <h2>Hey computer, please make a decision for me!</h2>
+            </div>
+        )
 
-let count = 0;
-var template2 = (
-    <div>
-    <h1>Count:{count}</h1>
-    <button id="my-id" className="class">+1</button>
-    </div>
-);
-var appRoot = document.getElementById('app');
-
-const multiplier = {
-    numbers:[1,2,3],
-    multiplyBy: 2,
-    multiply(){
-        return this.numbers.map((number)=>number*this.multiplyBy);
     }
-
 }
 
-console.log(multiplier.multiply());
+class Action extends React.Component{
+    render(){
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        )
+    }
+}
 
-ReactDOM.render(template,appRoot);
+const jsx = (
+    <div>
+        <Header />
+        <Action />
+    </div>
+
+);
+   
+
+
+ReactDOM.render(jsx,document.getElementById('app'));

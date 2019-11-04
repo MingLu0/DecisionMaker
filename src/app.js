25d34c4b6a70d0ad5a18e6cb1f096ebe9f7915f3
Ming Luo <luoming886@gmail.com>
2019-11-04 21:37:24 +0800

    set up webpack with scss

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DecisionMakerApp from './components/DecisionMakerApp';
+import './styles/styles.scss';
 
 
 ReactDOM.render(<DecisionMakerApp/>,document.getElementById('app'));
--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+//import Modal from 'react-modal'
 import Modal from 'react-modal';
 
 const OptionModal = (props) =>(
